c9013cf96864bbfd8e7de738fde95c071e2c5478
Patrick <pjmorgan716@gmail.com>
2022-02-10 10:55:34 -0500

    day3 start

---
+++ lectures/react/part1/day3/main.js
@@ -0,0 +1,18 @@
+// main.js
+
+// console.log(document)
+// console.dir(document) // dir gives us an object like representation of a thing that is not an object
+
+
+// console.log(document.body)
+
+
+
+
+
+
+
+
+
+
+
---
+++ lectures/react/part1/day3/notes.js
@@ -0,0 +1,40 @@
+// notes.js 
+
+// many unit tests comprises a singular functionality and then many functionalities comprise a larger unit of measuremant
+// end to end testing 
+
+// write our tests first 
+
+// TDD
+// PFR
+
+// tests act as documentation => should ultimately infer the code you end up writing 
+// (something should or should NOt hapen) {
+     // mock up and do taht something to prove your assertion
+// }
+
+
+
+
+// // production ready code
+
+// “Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler
+// Once a section has been completed:
+// - prepare your code to ‘push to production’ by: removing all extra development logs and unnecessary commented out code or comments, 
+// format the code according to your style guidelines, and refactor where and if necessary.
+// - push final code to your repo and set up a code review via Calendly with me
+// - prepare to defend your code in the code review: think of your solutions in terms of efficiency (BigO), are there any edge cases you have not thought of and do they need to be addressed, think of why you chose to do things a certain way or if you got stuck somewhere and created a unique solution be ready to discuss why you made that choice, ...etc.
+// - During the code review you may be asked to alter or expand on your code some come prepared with all files and folders open in your IDE ready to be shown and worked on.
+// - Remember to think out loud always, take your time when presented with new questions, up to an extent feel free to bounce questions or ideas of your reviewer, don’t get to stressed out if things aren’t going your way — writing bad code does not make you a bad programmer, it makes you human.
+
+
+
+
+
+
+
+
+
+
+
+
