a306306a0342c4c861f75735cfe969572acf7d53
Patrick <pjmorgan716@gmail.com>
2022-02-10 10:23:13 -0500

    end

---
+++ lectures/react/day0/notes.js
@@ -0,0 +1,31 @@
+// notes.js
+
+// engage
+
+// code reviews upcoming
+
+
+// afternoon assignment
+
+// 1st read: 
+// Learning Resource: Socket.IO and Testing Node
+// https://academy.engagelms.com/mod/scorm/player.php
+
+// Then: 
+// react day 0 assignment
+
+// Complete the following exercise by Tuesday morning the 8th of February at 10:00am est
+// handed in via the following github classroom link:
+// https://classroom.github.com/a/jpLeBodm
+
+// lab exercise 5 - Node.js
+// https://academy.engagelms.com/mod/page/view.php?id=63687
+
+// with the following changes:
+
+// Create an API use an array of fake data like we have been which should be able to perform CRUD operations on a JSON data.
+// API must be parametrized, and you must get individual values using param and query param. 
+// Using Mocha and Chai write a test case to test all endpoints of API.
+// Make sure the status of all the APIs result 200 OK. [aka are RESTfull]
+// Also write negative test case for all the APIs
+
