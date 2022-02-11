f4e03bb5e1134c5f2bdc2ab8ef83a781481beea2
Patrick <pjmorgan716@gmail.com>
2022-02-11 10:08:30 -0500

    first

---
+++ lectures/react/part1/day4/assignments/react-intro-assignments.js
@@ -0,0 +1,18 @@
+// react intro assignments
+
+// complete the following assignments and hand in via the following github classroom link:
+// https://classroom.github.com/a/PWDJ6eCT
+// These will be due Tuesday morning the 15th of December at 10:00am est
+// If you finish early and are wondering what to practice or how to prepare for the assessment then
+// 1st - fill in any knowledge gaps for node/express === all 6 routes and positive/negative test cases using mocha and chait
+// 2nd - practice algorithm questions on leetcode/edabit
+// 3rd - write tests for the following 3 assignments using Jest or enzyme if needed(I dont think you need it off the top of my head)
+
+// Assignment 1 - Timer App
+// https://academy.engagelms.com/mod/page/view.php?id=63645
+
+// Assignment 2 - Product Hunt
+// https://academy.engagelms.com/mod/page/view.php?id=63646
+
+// Assignment 3 - Calculator
+// https://academy.engagelms.com/mod/page/view.php?id=63647
