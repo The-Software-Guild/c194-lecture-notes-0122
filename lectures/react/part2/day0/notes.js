61fa6f2bf9a2813b1dab5ee89814903d95ef3e8f
Patrick <pjmorgan716@gmail.com>
2022-02-14 13:54:22 -0500

    first

--- lectures/react/part2/day0/notes.js
+++ lectures/react/part2/day0/notes.js
@@ -1,11 +1,25 @@
 // notes.js
 
+// constructor functions and classes get hoisted differently 
+
+
+// react setup and conventions
+// most working files live in the src
+// organization
+// do NOT want deeply nested things
+
+// naming 
 
 
 
+// React.js
+// allows you to make complex UI for each state of your applciation
+// rendering just the right compoents when your data changes
 
+// react uses somethign called the virtual DOM 
 
-
+// declarative & component-based
+// component is the smallest unit of a reusable piece of a website
 
 
 
