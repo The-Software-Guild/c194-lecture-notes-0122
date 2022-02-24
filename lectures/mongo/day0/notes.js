fe4364665780657f3dfe41928679c177dcfafc03
Patrick <pjmorgan716@gmail.com>
2022-02-24 11:35:10 -0500

    end

--- lectures/mongo/day0/notes.js
+++ lectures/mongo/day0/notes.js
@@ -12,6 +12,7 @@
 // to-do => resource (represented by a singular document)
 
 
+// MongoDB (non-relational) Basic
 // BASE (non-relational)-basic vs ACID (relational)-rigid
 // basically available 
 // soft-state 
@@ -23,6 +24,15 @@
 // Isolation => concurency (controlling things independently)
 // Durability => all changes made to DB are permanent once a singualr transaction is complete
 
+// MongoDB => unstructured data 
+// Mongoose (ODM)
+// models => add structure to our data 
+
+const schema = new mongoose.Schema({
+     name: 'string',
+     admin: 'boolean'
+});
+const User = mongoose.model('User', schema);
 
 
 
@@ -38,3 +48,25 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// blog posts => post
+// users => user
+// collection (to-do's for a user) is a group of similar resources (a singular to-d0) 
+// and a resource is a singular piece of data 
